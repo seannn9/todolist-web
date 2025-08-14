d05beee13baa6a215219902149af938953d759a3
Sean Ulric <seanulric9@gmail.com>
2025-08-14 22:57:28 +0800

    style: added small info for now

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,16 @@
 export default function About() {
-    return <div>About Page</div>;
+    return (
+        <section className="flex flex-col gap-4 h-full p-4">
+            <div className="w-full flex justify-center">
+                <h1 className="text-4xl text-primary font-bold">
+                    About Task Master
+                </h1>
+            </div>
+            <div className="flex justify-center">
+                <p className="text-xl">
+                    Task Master is a simple and minimalistic todo list.
+                </p>
+            </div>
+        </section>
+    );
 }
