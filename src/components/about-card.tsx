b94a37c4660a27dda8f43ac84fe4317ea2ebb61a
Sean Ulric <seanulric9@gmail.com>
2025-08-24 23:05:43 +0800

    style: adjust card size on 2xl screen

--- src/components/about-card.tsx
+++ src/components/about-card.tsx
@@ -4,7 +4,7 @@ interface AboutCardProps {
 }
 export default function AboutCard({ title, desc }: AboutCardProps) {
     return (
-        <article className="px-4 py-5 w-full lg:w-1/2 flex flex-col sm:flex-row justify-around gap-4 sm:gap-8 bg-sidebar border-2 border-border rounded-md">
+        <article className="px-4 py-5 w-full lg:w-10/12 2xl:w-1/2 flex flex-col sm:flex-row justify-around gap-4 sm:gap-8 bg-sidebar border-2 border-border rounded-md">
             <h2 className="text-lg self-center text-primary font-bold min-w-1/5 text-center">
                 {title}
             </h2>
